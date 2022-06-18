ddab540d265a0dbbb37f858f24505b5379f7a5d9
Danil-in-Github <105982365+Danil-in-Github@users.noreply.github.com>
2022-06-18 09:48:10 +0500

    Add files via upload

---
+++ zadanie_3.py
@@ -0,0 +1,19 @@
+def str_compare(A, B):
+    ngrams = [A[i:i+3] for i in range(len(A))]
+    count = 0
+    for ngram in ngrams:
+        count += B.count(ngram)
+
+    return count
+
+if __name__ == "__main__":
+    stroki = [("меч", "печь"), ("птица", "ученица"), ("университет", "специалитет"), ("exception", "interception")]
+    for s, t in stroki:
+        print(s, t, str_compare(s, t))
+
+print()
+
+str_const = "Я пришел домой поздно вечером"
+spisok = [500, "Завтрак", "Машина", str_const, "School", "Каникулы"]
+for elem in spisok:
+    print(elem)
